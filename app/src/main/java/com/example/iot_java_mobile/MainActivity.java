cc6663915da32c0337e289bec2e567c0bc5df199
BilenAssres <bilenassres@gmail.com>
2022-04-18 02:26:29 +0300

    Login Page validation check

--- app/src/main/java/com/example/iot_java_mobile/MainActivity.java
+++ app/src/main/java/com/example/iot_java_mobile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.iot_java_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -15,13 +16,75 @@ public class MainActivity extends AppCompatActivity {
     EditText name;
     EditText password;
     Button register;
-
+    EditText loginEmail;
+    EditText loginPassword;
+    Button login;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUI();
+        setupListeners();
+
+        private void setupUI() {
+            loginEmail = findViewById(R.id.et_email_login);
+            loginPassword = findViewById(R.id.et_password);
+            register = findViewById(R.id.btn_register);
+            login = findViewById(R.id.btn_login);
+        }
+        private void setupListeners() {
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkUsername();
+                }
+            });
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
+        }
+
+        void checkUsername() {
+            boolean isValid = true;
+            if (isEmpty(loginEmail)) {
+                loginEmail.setError("You must enter email to login!");
+                isValid = false;
+            } else {
+                if (!isEmail(loginEmail)) {
+                    loginEmail.setError("Enter valid email!");
+                    isValid = false;
+                }
+            }
+
+            if (isEmpty(loginPassword)) {
+                loginPassword.setError("You must enter password to login!");
+                isValid = false;
+            } else {
+                if (loginPassword.getText().toString().length() < 4) {
+                    loginPassword.setError("Password must be at least 4 chars long!");
+                    isValid = false;
+                }
+            }
+
+            if (isValid) {
+                String emailValue = loginEmail.getText().toString();
+                String passwordValue = password.getText().toString();
+                if (emailValue.equals("test@test.com") && passwordValue.equals("password1234")) {
+                    Intent i = new Intent(LoginActivity.this, FirstActivity.class);
+                    startActivity(i);
+                    this.finish();
+                } else {
+                    Toast t = Toast.makeText(this, "Wrong email or password!", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        }
 
         email = findViewById(R.id.et_email);
         name = findViewById(R.id.et_name);
